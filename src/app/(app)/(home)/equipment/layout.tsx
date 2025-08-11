ad15d9cf37942bab04294fc073612b5adf74933b
LytheanSem <lytheansem430@gmail.com>
2025-08-11 14:42:11 +0700

    05: category-filter-fixed

---
+++ src/app/(app)/(home)/equipment/layout.tsx
@@ -0,0 +1,42 @@
+import { Category } from "@/payload-types";
+import configPromise from "@payload-config";
+import { getPayload } from "payload";
+import { SearchFilters } from "../search-filter";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Layout = async ({ children }: Props) => {
+  const payload = await getPayload({
+    config: configPromise,
+  });
+
+  const data = await payload.find({
+    collection: "categories",
+    depth: 1, // Populate subcategories, subcategories.[0] will be a type of "Category"
+    pagination: false,
+    where: {
+      parent: {
+        exists: false,
+      },
+    },
+  });
+
+  const formattedData = data.docs.map((doc) => ({
+    ...doc,
+    subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+      // Because of "depth: 1" we are confident "doc" will be a type of "Category"
+      ...(doc as Category),
+      subcategories: undefined,
+    })),
+  }));
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <SearchFilters data={formattedData} />
+      <main className="flex-1 bg-[#F4F4F0]">{children}</main>
+    </div>
+  );
+};
+export default Layout;
