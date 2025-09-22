01741b783aed030c88c4f6624f21dcd6ee92a2ad
sapphirexseng <sapphire.seng77@gmail.com>
2025-09-22 22:23:59 +0700

    020:fixing admin stage booking

--- src/app/(app)/(home)/admin/stage-bookings/page.tsx
+++ src/app/(app)/(home)/admin/stage-bookings/page.tsx
@@ -12,6 +12,7 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { 
   Calendar, 
+  Clock,
   MapPin, 
   Users, 
   FileText, 
