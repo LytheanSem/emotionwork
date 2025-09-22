5adb1900fc5a111ba447238cb40bf634610d214f
sapphirexseng <sapphire.seng77@gmail.com>
2025-09-22 22:46:34 +0700

    020:fixing stage booking

--- src/app/(app)/(home)/api/stage-bookings/[id]/route.ts
+++ src/app/(app)/(home)/api/stage-bookings/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
-import { getDb } from "@/lib/db";
+import { getDb, StageBooking } from "@/lib/db";
 import { ObjectId } from "mongodb";
 
 export async function GET(
